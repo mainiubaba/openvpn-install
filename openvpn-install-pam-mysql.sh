2a5fab2a36575f5d1492f1a94715ef7c3a839001
Roles_le <13051960278@163.com>
2019-04-25 14:16:25 +0800

    sl

--- openvpn-install-pam-mysql.sh
+++ openvpn-install-pam-mysql.sh
@@ -4,7 +4,7 @@
 #
 # Copyright (c) 2013 Nyr. Released under the MIT License.
 
-
+echo ${TERM}
 # Detect Debian users running the script with "sh" instead of bash
 if readlink /proc/$$/exe | grep -q "dash"; then
 	echo "This script needs to be run with bash, not sh"
